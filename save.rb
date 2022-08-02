8a4bdbc122e3da18c0a9040f1d9030de83b9ac9c
kinginthenorthcodez <kinginthenorthcodez@gmail.com>
2022-08-03 04:33:18 +0530

    Add Read method file

--- save.rb
+++ save.rb
@@ -6,10 +6,10 @@ def save_person(app)
   persons = []
   app.persons.each do |person|
     if person.class.to_s == 'Student'
-      st = {name: person.name, age: person.age, permission: person.parent_permission}
+      st = {name: person.name, age: person.age, permission: person.parent_permission, type: person.class}
       persons.push(st)
     else
-      teacher = {name: person.name, age: person.age, specialization: person.specialization}
+      teacher = {name: person.name, age: person.age, specialization: person.specialization, type: person.class}
       persons.push(teacher)
     end
   end
@@ -23,4 +23,13 @@ def save_rental(app)
 end
 
 
-
+def read_person
+  persons = JSON.parse(File.read('person.json'))
+  persons.each do |person|
+    if person.type == 'Student'
+      @persons << Student.new('Unkown', person.age, person.name, person.parent_permission: permission)
+    else
+      @persons << Teacher.new(person.specialization, person.age, person.name)
+    end
+  end
+end
